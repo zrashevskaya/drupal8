3d19ae25784ab8c47d9e56a516b80ca053b335d3
zrashevskaya <zrashevskaya@adyax.com>
2016-11-09 19:16:07 +0200

    Some changes in module for routing testing

--- modules/custom/d8_routes/src/Routing/RouteSubscriber.php
+++ modules/custom/d8_routes/src/Routing/RouteSubscriber.php
@@ -3,7 +3,11 @@
 namespace Drupal\d8_routes\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\user\Controller\UserController;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
 
 /**
  * Listens to the dynamic route events.
@@ -16,6 +20,18 @@ class RouteSubscriber extends RouteSubscriberBase {
   public function alterRoutes(RouteCollection $collection) {
     if ($route = $collection->get('entity.user.canonical')) {
       $route->setOption('_admin_route', 'TRUE');
+    }
+
+    if ($route = $collection->get('entity.taxonomy_term.canonical')) {
+      if(!(\Drupal::currentUser()->hasPermission("administrator"))){
+        $route->addDefaults(['_title'=>'You are not administrator']);
+      };
+
+//      if(!(in_array('administrator', $user[$args]))) {
+//        $route->setOption('_admin_route', 'TRUE');
+//      }
+//      return \Drupal::service('url_generator')->generateFromRoute('system.404');
+//      $route->setPath(\Drupal::service('url_generator')->generateFromRoute('system.404'));
     }
   }
 
